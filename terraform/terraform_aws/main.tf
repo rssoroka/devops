24f3ceadd6b7179a3eb49943379a036f53702974
rssoroka <r.soroka.2020@alumnos.urjc.es>
2021-11-23 20:58:45 +0100

    pepe

--- terraform/terraform_aws/main.tf
+++ terraform/terraform_aws/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket  = "devops-urjc-state"
+    bucket  = "devops-urjc-state-roman"
     key     = "terraform/devops-swarm-state/terraform.tfstate"
     region = "eu-west-1"
 
